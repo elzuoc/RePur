09315fc839199f21c3e0474a2ed3b83d208b7063
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2022-10-06 16:46:49 +0800

    fix: eslint & vite setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,17 @@
+/* eslint-env node */
+
 module.exports = {
   root: true,
-  env: {
-    node: true,
-  },
-  parserOptions: {
-    ecmaVersion: 'latest',
-  },
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     'airbnb-base',
   ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'max-len': 'off',
   },
   settings: {
     'import/resolver': {
